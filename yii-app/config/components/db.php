d990205e88e690b6d535496de90137093872fee9
Yura Griga <grigach@gmail.com>
2015-06-21 12:48:35 +0300

    progress

--- yii-app/config/components/db.php
+++ yii-app/config/components/db.php
@@ -3,20 +3,20 @@
 if (getenv('OPENSHIFT_MYSQL_DB_HOST')) {
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=' . getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT') . ';dbname=m22cms',
+        'dsn' => 'mysql:host=' . getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT') . ';dbname=',
         'username' => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
         'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
         'charset' => 'utf8',
-        'tablePrefix' => 'm22_'
+        'tablePrefix' => ''
     ];
 } else {
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=m22cms',
+        'dsn' => 'mysql:host=localhost;dbname=',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',
-        'tablePrefix' => 'm22_'
+        'tablePrefix' => ''
     ];
 }
 
